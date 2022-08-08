22ddf7c4d732a8919f4602ce2c88e7e02f25a3dd
unknown <vaibhavpimpalkar28@gmail.com>
2022-08-09 02:37:58 +0530

    post-man

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,38 +1,81 @@
 const express = require('express');
-const abc = require('../introduction/intro')
+// const abc = require('../introduction/intro')
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    logger.welcome()
+// router.get('/test-me', function (req, res) {
+//     console.log('My batch is', abc.name)
+//     abc.printName()
+//     logger.welcome()
 
-    res.send('My second ever api!')
-});
+//     res.send('My second ever api!')
+// });
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+// problem 1
+router.get('/movies',function(req,res){
+    let movies=['mr.robot','valley','3 idiot','hero','pk']
+    res.send(movies)
 })
 
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
+// problem 2 & 3 both are same
+router.get('/movies/:indexNumber',function(req,res){
+    let movies=['mr.robot','valley','3 idiot','hero','pk']
+    let reqparam=req.params
+    let i=reqparam.indexNumber
+    if(i<movies.length){
+        return res.send(movies[i])
+    }
+    else{
+ return res.send('use valid index')
+    }
 })
+
+// problem 4
+
+router.get('/films',function (req,res) {
+    const film=[ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+
+       res.send(film)
+       
+})
+
+
+// problem 5
+
+router.get('/films/:filmId',function(req,res) {
+    const film=[ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       const reqparam=req.params
+       let i=reqparam.filmId
+       if(i<film.length){
+        return res.send(film[i])
+       }
+       else{
+        return res.send('No movie exists with this id')
+       }
+})
+
 
 module.exports = router;
