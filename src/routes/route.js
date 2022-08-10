71eb4f25d34a3d6b6fa98aa327c7c084723667d6
unknown <vaibhavpimpalkar28@gmail.com>
2022-08-11 00:20:43 +0530

    post request

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,46 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+let players=[
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": [
+            "swimming"
+        ]
+    },
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": [
+            "soccer"
+        ]
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ]
+    },
+]
+
+ router.post("/players",function(req,res){
+    let newplayer=req.body.name
+    for(let i=0;i<players.length;i++)
+    if(players[i].name=== newplayer){
+        res.send({msg:"the newplayer is already exist"})
+    }
+    players.push(req.body)
+    res.send(players)
+ })
+
+
+
 module.exports = router;
