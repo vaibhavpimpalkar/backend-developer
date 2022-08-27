dd573a2fec76842f1592d5ef239237118d34e8b2
unknown <vaibhavpimpalkar28@gmail.com>
2022-08-27 12:35:29 +0530

    middleware

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,55 +1,18 @@
-const UserModel= require("../models/userModel")
+const UserModel = require("../models/userModel")
 
-
-
-
-const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
+const basicCode = async function (req, res) {
+    let tokenDataInHeaders = req.headers.token
     console.log(tokenDataInHeaders)
 
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    res.send({ msg: "This is coming from controller (handler)"})
-    }
-
-
-const commonHandler = async function(req, res, next) {
-    console.log('I am inside the common route handler')
-    next()
-    //res.send({status: true, msg: "Hi there!"})
+    console.log("HEADER DATA ABOVE")
+    console.log("hey man, congrats you have reached the Handler")
+    res.send({ msg: "This is coming from controller (handler)" })
 }
 
-module.exports.commonHandler = commonHandler
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
+const createUser = async function (req, res) {
+    let data = req.body
+    let savedData = await UserModel.create(data);
+    res.send({ msg: savedData })
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
-
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
-module.exports.basicCode= basicCode
+module.exports = { createUser, basicCode };
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,70 +1,21 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
-const commonMW = require ("../middlewares/commonMiddlewares")
+
+const { createUser, getUsersData, basicCode, abc } = require("../controllers/userController")
+const createBook = require("../controllers/bookController")
+const { mid1, mid2, mid3, mid4 } = require("../middlewares/commonMiddlewares")
+
+
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
+router.post("/createBook", createBook)
 
+router.post("/createUser", createUser  );
 
-
-router.post("/createBook", BookController.createBook  )
-
-
-
-
-// router.post("/createUser", UserController.createUser  )
-// router.get("/getUsersData", UserController.getUsersData)
-
-
-const mid1= function ( req, res, next) {
-    console.log ("inside GLOBAL MW");
-    console.log("Hi I am a middleware named Mid1")
-    // logic
-    let loggedIn = false
-
-    if (loggedIn== true) { 
-        console.log( "OK LOGGED IS IS TRUE NOW")
-        next ()
-    }
-    else {
-        res.send ("Please login or register")
-    }
-}
-
-/*
-
-*/
-
-// e.g. restricted and open-to-all API's can be handled like below now:
-router.get('/homePage', mid1, UserController.commonHandler)
-router.get('/profileDetails', mid1, UserController.commonHandler)
-router.get('/friendList', mid1, UserController.commonHandler)
-router.get('/changePassword', mid1, UserController.commonHandler)
-
-router.get('/termsAndConditions',  UserController.commonHandler)
-router.get('/register',  UserController.commonHandler, function(req, res){
-    console.log('This is last console statement')
-    res.send({status: true, msg: "Am ending the cycle."})
-})
-
-
-
-
-
-//router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
-
-
-
-// router.get("/basicRoute2", commonMW.mid1, UserController.basicCode2)
-// router.get("/basicRoute3", commonMW.mid2, UserController.basicCode3)
-// router.get("/basicRoute4", commonMW.mid1, commonMW.mid4, UserController.basicCode4)
-
-
-
+router.get("/basicRoute", mid1, mid2, mid3, mid4, basicCode)
 
 module.exports = router;
